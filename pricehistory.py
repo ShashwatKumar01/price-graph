dae3c4d26a145e3118ffc29c1ef911400db03601
Shashwat  Kumar Das <121427522+ShashwatKumar01@users.noreply.github.com>
2024-03-08 11:29:05 +0530

    Update pricehistory.py

--- pricehistory.py
+++ pricehistory.py
@@ -14,25 +14,16 @@ bot = Quart(__name__)
 async def hello():
     return 'Hello, world!'
 
-@app.on_message(filters.command("start") & (filters.private | filters.group))
+@app.on_message(filters.command("start") & filters.private)
 async def start(client, message):
     # Check if the message is in a group
 
     if message.chat.type== enums.ChatType.PRIVATE:
         await message.reply_text(
             "Hey! Just send me a valid Amazon product link. I will share you the Price History Graph 😍😍")
-    else:
-        chat_member = await client.get_chat_member(message.chat.id, message.from_user.id)
-        is_admin = chat_member.status in ["administrator", "OWNER"]
-        if is_admin:
-            await message.reply_text(
-                "Hey! Just send me a valid Amazon product link. I will share you the Price History Graph 😍😍")
-        else:
-            await message.reply_text("Sorry, only group admins can use this command.")
-
-@app.on_message(filters.private | filters.group)
+@app.on_message(filters.private)
 async def handle_text(client, message):
-    a=await app.send_message(message.chat.id,"Just wait 5 Seconds⏳⏳....Bot is Working🤖>>>>")
+
     try:
         if message.photo:
             # Extract the text and media link from the message
@@ -41,24 +32,27 @@ async def handle_text(client, message):
             # print("Media Link:", media_link)
             # await app.send_message(message.chat.id, "Text and media link extracted successfully.")
         elif message.text:
+
             inputvalue=message.text
 
-        # print(inputvalue)
+        # print(inputvalue,message.from_user.id)
     except Exception as e:
         # Handle exceptions
         await app.send_message(message.chat.id, f"Something went wrong: {str(e)}")
 
     try:
+        if 'LivegramBot'in inputvalue or 'You cannot forward someone' in inputvalue:
+            return None
         extracted_link=extract_link_from_text(inputvalue)
         # print(extracted_link)
         if not extracted_link:
             d = await app.send_message(message.chat.id, "Link not Found🫥🫥...")
             await asyncio.sleep(4)
-            await a.delete()
+            # await a.delete()
             await d.delete()
             return None
         clean_url = remove_amazon_affiliate_parameters(unshorten_url(extracted_link))
-
+        a = await app.send_message(message.chat.id, "Just wait 5 Seconds⏳⏳....Bot is Working🤖>>>>")
         product_name, imageUrl,Price = await get_product_details(clean_url)
         # print("Product Name:", product_name)
         # print('imageUrl: ' + imageUrl)
@@ -84,8 +78,8 @@ async def handle_text(client, message):
         user_info = f"User ID: {message.from_user.id}\nUsername: @{message.from_user.username}\nUser Input: {message.text}"
         error_message = f"Error: {str(e)}\n\nUser Info:\n{user_info}"
         contact_admin_button = InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Contact Admin", url="https://t.me/shashwatkumar01",)]])
-        b = await app.send_message(admin_chat_id, error_message, reply_markup=contact_admin_button)
+            [[InlineKeyboardButton("Contact Admin", url="https://t.me/imovies_contact_bot",)]])
+        b = await app.send_message(admin_chat_id, error_message)
         user_error_message = f"Oops! Something went Wrong.Input only Amazon Product URL.. Try Again.Reported to the admin."
         b= await app.send_message(message.chat.id, user_error_message,reply_markup=contact_admin_button)
         await asyncio.sleep(10)
